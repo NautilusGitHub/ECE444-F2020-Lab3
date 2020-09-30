eadbca51d6d3fdc26aeea5043f7b1ccb9844a321
NautilusGitHub <jasonshang64@gmail.com>
2020-09-30 11:23:35 -0400

    Example 4-4: created basic name form

--- hello.py
+++ hello.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'perhaps you might not guess this string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 # http://127.0.0.1:5000/
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+    form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 # dynamic route: http://localhost:5000/user/name
 @app.route('/user/<name>')
