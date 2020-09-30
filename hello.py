d6272dfcafdaa0a5a0beabd28f4c4cc68eeb70cb
NautilusGitHub <jasonshang64@gmail.com>
2020-09-30 11:31:55 -0400

    Example 4-5: refreshes now work properly due to redirect added. Replaced depreciated Form with FlaskForm

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
@@ -18,12 +18,11 @@ moment = Moment(app)
 # http://127.0.0.1:5000/
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-    form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 # dynamic route: http://localhost:5000/user/name
 @app.route('/user/<name>')
